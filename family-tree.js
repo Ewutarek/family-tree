693baaebaef46ec40d79c3746a2181d67380386e
Keratuwe <mokhantsopheko@gmail.com>
2020-09-23 16:19:14 +0100

    Test for family tree and generations

--- family-tree.js
+++ family-tree.js
@@ -1,4 +1,25 @@
-const me = {name: 'Keratuwe'}
-const cousin = {name: 'Ntsana'}
+/*-----------------------------Initialised Family members-----------------------------*/ 
+const me = {name: 'Keratuwe', parents:[], generation: 1}
+const cousin = {name: 'Ntsana', parents:[], generation: 1}
+const mum = {name: 'Lebohang', parents: [], generation: 2 }
+const aunt1 = {name:'Mohau', parents: [], generation: 2 }
+const aunt2 = {name:'Mamello', parents: [], generation: 2  }
+const grandma = {name: 'Ellen', generation: 3 }
+const grandpa = {name: 'Ernest', generation: 3 }
 
-module.exports = [me, cousin]
+/*-----------------------------Establish parents-------------------------------------*/ 
+me.parents[0] = mum
+cousin.parents[0] = aunt1
+
+aunt2.parents[0] = grandma
+aunt2.parents[1] = grandpa
+
+aunt1.parents[0] = grandma
+aunt1.parents[1] = grandpa
+
+mum.parents[0] = grandma
+mum.parents[1] = grandpa
+
+
+/*--------------------------Export Module---------------------------------------------*/ 
+module.exports = [me, cousin, mum, aunt1, aunt2,grandma,grandpa]
--- family-tree.test.js
+++ family-tree.test.js
@@ -14,4 +14,81 @@ describe('Family Tree', function ()
         expect(cousin.name).toEqual('Ntsana')
     })
 
+    test('Mum is also there', function() 
+    {
+        const mum = familyTree[0].parents[0]
+        expect(mum.name).toEqual('Lebohang')
+    })
+
+    test('Oldest Aunt is also there', function() 
+    {
+        const aunt1 = familyTree[1].parents[0]
+        expect(aunt1.name).toEqual('Mohau')
+    })
+
+    test('Middle Aunt is also there', function() 
+    {
+        const aunt2= familyTree[4]
+        expect(aunt2.name).toEqual('Mamello')
+    })
+
+    test('Grandma is also there', function() 
+    {
+        const grandma = familyTree[4].parents[0]
+        expect(grandma.name).toEqual('Ellen')
+    })
+
+    test('Grandpa is also there', function() 
+    {
+        const grandpa = familyTree[2].parents[1]
+        expect(grandpa.name).toEqual('Ernest')
+    })
+
+
+
+})
+
+describe('Generations', function () 
+{
+    test('I am in 1st Generation', function() 
+    {
+        const me = familyTree[0]
+        expect(me.generation).toEqual(1)
+    })
+
+    test('Cousin is also 1st Generation', function() 
+    {
+        const cousin= familyTree[1]
+        expect(cousin.generation).toEqual(1)
+    })
+
+    test('Mum is 2nd Generation', function() 
+    {
+        const mum = familyTree[0].parents[0]
+        expect(mum.generation).toEqual(2)
+    })
+
+    test('Oldest Aunt is also 2nd Generation', function() 
+    {
+        const aunt1 = familyTree[1].parents[0]
+        expect(aunt1.generation).toEqual(2)
+    })
+
+    test('Middle Aunt is also 2nd Generation', function() 
+    {
+        const aunt2= familyTree[4]
+        expect(aunt2.generation).toEqual(2)
+    })
+
+    test('Grandma is 3rd Generation', function() 
+    {
+        const grandma = familyTree[4].parents[0]
+        expect(grandma.generation).toEqual(3)
+    })
+
+    test('Grandpa is also 3rd Generation', function() 
+    {
+        const grandpa = familyTree[2].parents[1]
+        expect(grandpa.generation).toEqual(3)
+    })
 })
